d63fab44bfec60659a97bc09fbe015d17b52da8f
Vinay Vishwakarma <vinay.vishwakarma@accenture.com>
2020-08-11 20:35:43 +0100

    enable VPC flow logs

--- enable_vpc_flowlogs.tf
+++ enable_vpc_flowlogs.tf
@@ -1,7 +1,7 @@
 
 module "enable_vpc_flowlogs_cmk" {
   source = "./modules"
-  vpc_id = "vpc-xxxxxxx"
+  vpc_id = "vpc-xxxxxxx" # Enter your VPC ID
   region = "ap-south-1"
   retention_in_days = 7
 }
